7a91657ee0f4d324623beecce8f776274837bee3
Daniel <ddiegow@uoc.edu>
2023-02-02 01:50:40 +0900

    Refactor permutations so it actually works

--- module2/src/main/java/Permutation.java
+++ module2/src/main/java/Permutation.java
@@ -3,13 +3,15 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
     public static void main(String[] args) {
-        int permutations = StdIn.readInt();
-        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<String>();
-        while (!StdIn.isEmpty()) {
-            randomizedQueue.enqueue(StdIn.readString());
-        }
-        while (permutations >= 0) {
-            StdOut.println(randomizedQueue.dequeue());
+        if (args.length == 1) {
+            int permutations = Integer.parseInt(args[0]);
+            RandomizedQueue<String> randomizedQueue = new RandomizedQueue<String>();
+            while (!StdIn.isEmpty()) {
+                randomizedQueue.enqueue(StdIn.readString());
+            }
+            while (permutations >= 0) {
+                StdOut.println(randomizedQueue.dequeue());
+            }
         }
     }
 }
