606395b33b1cb5b2237568177197ab96486a6ebf
Daniel <ddiegow@uoc.edu>
2023-02-20 16:03:33 +0900

    Implement all board methods.  Still have to check why neighbours is creating boards with mixed-up positions

--- module4/Board.java
+++ module4/Board.java
@@ -4,6 +4,8 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.Stack;
+
 public class Board {
     private int[][] board;
     private int n;
@@ -25,13 +27,22 @@ public class Board {
         if (tiles.length == 0) {
             throw new IllegalArgumentException();
         }
-        board = tiles;
         n = tiles.length;
+        board = tiles.clone();
     }
 
     // string representation of this board
+    @Override
     public String toString() {
-        return null;
+        StringBuilder s = new StringBuilder();
+        s.append(n + "\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                s.append(String.format("%2d ", board[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
     }
 
     // board dimension n
@@ -93,7 +104,39 @@ public class Board {
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return null;
+        Stack<Board> iterable = new Stack<>();
+        Coordinates zero = findZero();
+
+        if (zero.i > 0) { // we can look below
+            int[][] boardCopy = board.clone();
+            int tmp = boardCopy[zero.i][zero.j];
+            boardCopy[zero.i][zero.j] = boardCopy[zero.i - 1][zero.j];
+            boardCopy[zero.i - 1][zero.j] = tmp;
+            iterable.push(new Board(boardCopy));
+        }
+        if (zero.i < n - 1) { // we can look above
+            int[][] boardCopy = board.clone();
+            int tmp = boardCopy[zero.i][zero.j];
+            boardCopy[zero.i][zero.j] = boardCopy[zero.i + 1][zero.j];
+            boardCopy[zero.i + 1][zero.j] = tmp;
+            iterable.push(new Board(boardCopy));
+        }
+        if (zero.j > 0) { // we can look to the left
+            int[][] boardCopy = board.clone();
+            int tmp = boardCopy[zero.i][zero.j];
+            boardCopy[zero.i][zero.j] = boardCopy[zero.i][zero.j - 1];
+            boardCopy[zero.i][zero.j - 1] = tmp;
+            iterable.push(new Board(boardCopy));
+        }
+        if (zero.j < n - 1) { // we can look to the right
+            int[][] boardCopy = board.clone();
+            int tmp = boardCopy[zero.i][zero.j];
+            boardCopy[zero.i][zero.j] = boardCopy[zero.i][zero.j + 1];
+            boardCopy[zero.i][zero.j + 1] = tmp;
+            iterable.push(new Board(boardCopy));
+        }
+
+        return iterable;
     }
 
     // a board that is obtained by exchanging any pair of tiles
@@ -114,11 +157,27 @@ public class Board {
         return new Coordinates(i, j);
     }
 
+    private Coordinates findZero() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    return new Coordinates(i, j);
+                }
+            }
+        }
+        return null;
+    }
+
     // unit testing (not graded)
     public static void main(String[] args) {
         Board board = new Board(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } });
         System.out.println(board.hamming());
         System.out.println(board.manhattan());
         System.out.println(board.isGoal());
+        System.out.println(board);
+        Iterable<Board> it = board.neighbors();
+        for (Board b : it) {
+            System.out.println(b);
+        }
     }
 }
